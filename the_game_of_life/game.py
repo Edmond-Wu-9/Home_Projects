18e3b27c232dee12336a7c5170d5460105c4d337
Edmond-Wu-9 <115105345+Edmond-Wu-9@users.noreply.github.com>
2024-10-24 18:30:03 -0400

    Added cells and some key stroke functions

--- the_game_of_life/game.py
+++ the_game_of_life/game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #Initialize PyGame
 pygame.init()
@@ -23,7 +24,14 @@ pygame.display.set_caption("Edmond's Game of Life")
 # Create a clock 
 clock  = pygame.time.Clock()
 
+def gen(num):
+    return set([(random.randrange(0, GRID_HEIGHT), random.randrange(0,GRID_WIDTH)) for x in range(num)])
+
 def draw_grid(positions):
+    for position in positions:
+        col,row = position
+        top_left = (col * CELL_SIZE, row * CELL_SIZE)
+        pygame.draw.rect(screen, RED, (*top_left, CELL_SIZE , CELL_SIZE))
     for row in range(GRID_HEIGHT):
         pygame.draw.line(screen, BLACK, (0, row * CELL_SIZE),(WIDTH, row * CELL_SIZE))
     for col in range (GRID_WIDTH):
@@ -31,12 +39,29 @@ def draw_grid(positions):
 
 def main():
     running = True
+    playing = False
     positions = set()
+    positions.add((10,10))
     while running:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                col, row = pos[0] // CELL_SIZE, pos[1] // CELL_SIZE
+                if (col, row) in positions:
+                    positions.remove((col, row))
+                else:
+                    positions.add((col, row))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    playing = not playing
+                if event.key == pygame.K_c:
+                    positions.clear()
+                    playing = False
+                if event.key == pygame.K_g:
+                    positions = gen(random.randrange(2,5) * GRID_WIDTH)
         screen.fill(GREY)
         draw_grid(positions)
         pygame.display.update()
